a0ff1442f5defb84a3e2e864cc4ccd20440d0265
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-14 13:45:58 -0700

    Project setup and installed reqd dependencies

---
+++ baseball-stadium/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Baseball Stadium</h3>
+    </div>
+  );
+}
+
+export default App;
